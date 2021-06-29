5c62a0e42f7a26ffecf7e14cbf99aec61e70870c
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-06-29 07:57:38 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = caption + "@Tamilblasters_cc",
+                caption = caption + \n<b>"POWERED BY : @TAMILBLASTERS_CC + \n JOIN : @TRVPN "</b>,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
