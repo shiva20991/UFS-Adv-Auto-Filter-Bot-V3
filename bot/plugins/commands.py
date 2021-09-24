0749cf4ae1560ebed5097dbbe157105ec2060726
shiva20991 <80451254+shiva20991@users.noreply.github.com>
2021-09-24 14:08:00 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -12,7 +12,7 @@ db = Database()
 
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-update_channel = "@DevelopedBots"
+update_channel = FORCESUB_CHANNEL
     if update_channel:
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
